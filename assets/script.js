c3311a94b36b0e6011f8e2c6a886aa0a74140ae6
William Bryan <wwbryan00@gmail.com>
2021-01-07 02:07:49 -0800

    make search button generate city names

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
 
+// SEARCH BUTTON EVENT LISTENER
 $(".search").on("click", function() {
     var cityName = ($(".cityName").val()).toLowerCase();
     console.log("City Entered: " + cityName);
     
+    makeCity(cityName);
 });
 
+// GENERATES CITY ACCORDING TO USER INPUT THEN APPENDS IT TO SIDEBAR
+function makeCity(cityName) {
+    cityName = cityName.toUpperCase();
+    var cityBtn = $('<button>');
+    cityBtn.attr("class", "list-group-item list-group-item-action");
+    cityBtn.text(cityName);
 
-
+    $(".sidebarBody").append(cityBtn);
+}
 
 
 

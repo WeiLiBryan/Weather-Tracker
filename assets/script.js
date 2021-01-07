4a6d791ed362da102bdab869d1509c45c5d47fb4
William Bryan <wwbryan00@gmail.com>
2021-01-07 01:58:41 -0800

    add event listener to search

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+$(".search").on("click", function() {
+    var cityName = ($(".cityName").val()).toLowerCase();
+    console.log("City Entered: " + cityName);
+    
+});
+
+
+
+
+
+
+
+
+
+});
